c276597bbf2685a06121a51d2c98161bc44a9569
wahibhumzahwakil00 <50239328+wahibhumzahwakil00@users.noreply.github.com>
2020-11-29 12:56:55 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,8 @@
+navbar = document.querySelector('.navbar')
+Subsribe = document.querySelector('.rightNav')
+
+
+.addEventListener('click', ()=>{
+	navbar.classList.toggle('h-nav-resp');
+	Subsribe.classList.toggle('v-class-resp');
+}
